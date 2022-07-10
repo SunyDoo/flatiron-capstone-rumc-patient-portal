1aa852224497f2ae701b31f426207d1f81d44b8d
Sunil <snilavarath@gmail.com>
2022-07-10 16:59:17 -0400

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :locations, only: [:show, :index]
   resources :appointments
   resources :patients, only: [:show, :create, :index]
-  post "/signup", to: "patients#create"
-  post "/login", to: "sessions#create"
-  get "/auth", to: "patients#show"
-  delete "/logout", to: "sessions#destroy"
+  # post "/signup", to: "patients#create"
+  # post "/login", to: "sessions#create"
+  # get "/auth", to: "patients#show"
+  # delete "/logout", to: "sessions#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
