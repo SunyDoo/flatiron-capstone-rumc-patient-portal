cd8d6582e4878e86c2fe680ea2c5e5543623038b
Sunil <snilavarath@gmail.com>
2022-07-10 15:35:47 -0400

    usecontext started

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,47 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { config } from "./Constants";
+import { UserContext } from "./UserContext";
 
 function App() {
   const [locations, setLocations] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const msg = useContext(UserContext);
+  useEffect(() => {
+    fetch(`${config.url}/locations`)
+      .then((r) => r.json())
+      .then((data) => setLocations(data));
+  }, []);
 
   useEffect(() => {
-    const URL = config.url;
-    fetch(`${URL}/locations`)
+    fetch(`${config.url}/doctors`)
       .then((r) => r.json())
-      .then((data) => setLocations(data))
+      .then((data) => setDoctors(data));
   }, []);
 
   console.log(`Running in ${process.env.NODE_ENV}`);
 
   return (
     <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            {locations.map((location) => (
-              <li key={location.id}>{location.name}</li>
-            ))}
-          </Route>
-        </Switch>
-      </div>
+      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+        <div className="App">
+          <Switch>
+            <Route path="/doctors">
+              {doctors.map((doctor) => (
+                <li key={doctor.id}>{doctor.name}</li>
+              ))}
+            </Route>
+            <Route path="/">
+              {locations.map((location) => (
+                <li key={location.id}>{location.name}</li>
+              ))}
+              <p>{msg}</p>
+            </Route>
+          </Switch>
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// import { useState, createContext } from "react";
+// const UserContext = createContext()
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
