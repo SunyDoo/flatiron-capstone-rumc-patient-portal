f8febcc5426018c18ec4bb2d7446c3ee38815c0d
Sunil <snilavarath@gmail.com>
2022-07-15 13:12:26 -0400

    mdb ui added

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 
 // import { useState, createContext } from "react";
 // const UserContext = createContext()
