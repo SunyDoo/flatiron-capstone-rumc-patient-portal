8dac5021a5a83206e6d18fe776e34323b1a2839e
Sunil <snilavarath@gmail.com>
2022-07-15 13:11:18 -0400

    doctor filter added

--- client/src/Components/Doctors.js
+++ client/src/Components/Doctors.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import FilterDocs from "./FilterDocs";
+import {
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
 
-function Doctors({ doctors }) {
+function Doctors({ doctors }) { 
+  const [selectedDepartment, setSelectedDepartment] = useState("All");
+
+  const docsToDisplay = doctors.filter((doc) => {
+    if (selectedDepartment === "All") return true;
+
+    return doc.specialty === selectedDepartment;
+  })
+  
   return (
     <>
-      <div>
+      <FilterDocs
+        selectedDepartment={selectedDepartment}
+        setSelectedDepartment={setSelectedDepartment}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <ul>
-          {doctors.map((doc) => (
-            <li key={doc.id}>{doc.name}</li>
+          {docsToDisplay.map((doc) => (
+            <MDBCard style={{ maxWidth: "540px" }} key={doc.id}>
+              <MDBRow className="g-0">
+                <MDBCol md="4">
+                  <MDBCardImage src={doc.image} alt={doc.name} fluid />
+                </MDBCol>
+                <MDBCol md="8">
+                  <MDBCardBody>
+                    <MDBCardTitle>{doc.name}</MDBCardTitle>
+                    <MDBCardText>{doc.specialty}</MDBCardText>
+                    <MDBCardText>
+                      <small className="text-muted">
+                        {doc.location.address}
+                      </small>
+                      <br></br>
+                      <small className="text-muted">{doc.phone_number}</small>
+                    </MDBCardText>
+                  </MDBCardBody>
+                </MDBCol>
+              </MDBRow>
+            </MDBCard>
           ))}
         </ul>
       </div>
-      
     </>
   );
 }
---
+++ client/src/Components/FilterDocs.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function FilterDocs({ selectedDepartment, setSelectedDepartment }) {
+  return (
+    <div>
+        <h3>Filter By Department</h3>
+      <select
+        name="filter"
+        value={selectedDepartment}
+        onChange={(e) => setSelectedDepartment(e.target.value)}
+      >
+        <option value="All">All Departments</option>
+        <option value="Critical Care">Critical Care</option>
+        <option value="OB/GYN">OB/GYN</option>
+        <option value="Gastroenterology">Gastroenterology</option>
+        <option value="General Surgery">General Surgery</option>
+        <option value="Oncology">Oncology</option>
+        <option value="Cardiology">Cardiology</option>
+        <option value="Family Practice">Family Practice</option>
+        <option value="Psychiatry">Psychiatry</option>
+        <option value="Urology">Urology</option>
+        <option value="Pulmonary">Pulmonary</option>
+        <option value="Endocrinology">Endocrinology</option>
+        <option value="Medicine">Medicine</option>
+      </select>
+    </div>
+  );
+}
+
+export default FilterDocs;
