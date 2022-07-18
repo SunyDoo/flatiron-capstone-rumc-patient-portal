bf6560740c14b0bcbec7b3827bb62b70d138b71f
Sunil <snilavarath@gmail.com>
2022-07-17 22:11:35 -0400

    fixed footer

--- client/src/Components/FooterPage.js
+++ client/src/Components/FooterPage.js
@@ -26,8 +26,8 @@ export default function App() {
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         &copy; {new Date().getFullYear()} Copyright:{" "}
-        <a className="text-white" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+        <a className="text-white" href="https://flatironschool.com/">
+          Flatiron School
         </a>
       </div>
     </MDBFooter>
